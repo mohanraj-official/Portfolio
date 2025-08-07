8de1b48e6f02999511863a3834a4b659cca19360
MOHANRAJ S <121940251+mohanraj-official@users.noreply.github.com>
2025-08-07 17:43:37 +0530

    again testimonal added after correction of the scroll to top

--- script.js
+++ script.js
@@ -104,4 +104,16 @@ function openPopup(id) {
 
 
 
+/* testimonial swipes */
+  const swiper = new Swiper('.testimonial-swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+Code Duplicate Finder
 
